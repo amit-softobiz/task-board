87251b987c4b3ab9a5aef870749ebcfdf6d5ccf4
Amit Thakur <amit.thakur@softobiz.com>
2023-04-20 20:36:14 +0530

    add error modal component

--- src/components/tasks/Addtask.js
+++ src/components/tasks/Addtask.js
@@ -1,48 +1,48 @@
 import React,{useRef, useState} from 'react'
 import Card from '../UI/Card'
 import classes from './Addtask.module.css'
+import ErrorModal from '../UI/ErrorModal';
 const Addtask = (props) => {
     const titleInputRef = useRef();
-    const statusInputRef = useRef();
     const descriptionInputRef = useRef();
     const dateInputRef = useRef();
     const [error, setError] = useState();
 
     const addUserHandler = (event)=>{
         const titleref=titleInputRef.current.value;
-        const statusref=statusInputRef.current.value;
+        const status = "Active"
         const descriptionref=descriptionInputRef.current.value;
         const dateref=dateInputRef.current.value;
         event.preventDefault();
-        if(titleref.trim().length === 0 || statusref.trim().length ===0 || descriptionref.trim().length === 0 || dateref.trim().length ===0){
+        if(titleref.trim().length === 0 || descriptionref.trim().length === 0 || dateref.trim().length ===0){
             setError({
                 title:'Invalid Input',
-                message : 'Please enter a valid title,status, description and date (non-empty values).'
+                message : 'Please enter a valid title description and date (non-empty values).'
             })
             return;
         }
-        props.onAddTask(titleref,statusref,descriptionref,dateref);
+        props.onAddTask(titleref,status,descriptionref,dateref);
         titleInputRef.current.value='';
-        statusInputRef.current.value='';
         descriptionInputRef.current.value='';
         dateInputRef.current.value='';
      }
+     const errorHandler=()=>{
+        setError(null);
+    }
   return (
     <div>
-       <Card className={classes.input}>
+        {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
+    <Card className={classes.input}>
     <form onSubmit={addUserHandler}>
       <label htmlFor='title'>Title</label>
       <input id="title" type="text" ref={titleInputRef}/>
-
-      <label htmlFor='status'>Status</label>
-      <input id="status" type="text" ref={statusInputRef}/>
-
+  
       <label htmlFor='description'>Description</label>
       <input id="description" type="text" ref={descriptionInputRef}/>
 
       <label htmlFor='date'>Date</label>
-      <input id="date" type="text" ref={dateInputRef}/>
-
+      <input type='date' min='2019-01-01' max="2022-12-31" ref={dateInputRef} />
+      
       <button type="submit">Add Task</button>
     </form>
     </Card>
--- src/components/tasks/TaskList.js
+++ src/components/tasks/TaskList.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React,{useRef,useState} from 'react'
 import Card from '../UI/Card'
 import classes from './TaskList.module.css'
 const TaskList = (props) => {
+    const statusInputRef = useRef();
+    const [status, setStatus] = useState();
+    const filterStatusHandler = (event)=>{
+        const statusref=statusInputRef.current.value;
+        setStatus(statusref);
+    }    
+    const getTasksByStatus = (status) =>{
+    props.tasks.filter(task => task.status === status);
+    console.log()
+    }
+
+
   return (
     <Card className = {classes.tasks}>
+
+          <select id="status" value="Active" onChange={filterStatusHandler} ref={statusInputRef}>
+            <option value="active">Active</option>
+            <option value="inprogress">In Progress</option>
+            <option value="complete">Complete</option>
+          </select>
+
     <ul key={props.id}>
       {props.tasks.map(task =>(
+        
+
       <li>
-          {task.title} {task.status}{task.description}{task.date}
-      </li>))}
+          {task.title}   {task.status}   {task.description}   {task.date}
+      </li>
+      ))}
     </ul>
     </Card>
   )
