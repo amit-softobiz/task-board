24b41cf986b0d78f0266cfb19447aa3a81a88638
Amit Kumar <amit.thakur@softobiz.com>
2023-04-21 13:40:33 +0530

    add remove functioality on remove button

--- src/components/tasks/TaskList.js
+++ src/components/tasks/TaskList.js
@@ -1,48 +1,66 @@
-import React,{useRef,useState,useEffect} from 'react'
-import Card from '../UI/Card'
-import classes from './TaskList.module.css'
-
+import React, { useRef, useState, useEffect } from "react";
+import Card from "../UI/Card";
+import classes from "./TaskList.module.css";
+import Button from "../UI/Button";
 const TaskList = (props) => {
-    const statusInputRef = useRef();
-    const [check,setCheck]= useState(false);
-    const [status, setStatus] = useState('Active');
-    const [filteredData, setFilteredData] = useState([]);
-    useEffect(()=>{
+  const statusInputRef = useRef();
+  const [check, setCheck] = useState(false);
+  const [status, setStatus] = useState("Active");
+  const [filteredData, setFilteredData] = useState([]);
+  const [originalData, setOriginaldata] = useState(props.tasks);
+  useEffect(() => {}, []);
 
-    },[])
-
-    const filterStatusHandler = (event)=>{
-        const statusref=statusInputRef.current.value;
-        setStatus(statusref);
-        setCheck(true);
-        const filtered = props.tasks.filter(item => item.status === event.target.value);
-        setFilteredData(filtered);
-    }    
-
+  const filterStatusHandler = (event) => {
+    const statusref = statusInputRef.current.value;
+    setStatus(statusref);
+    setCheck(true);
+    const filtered = originalData.filter(
+      (item) => item.status === event.target.value
+    );
+ 
+    setFilteredData(filtered);
+  };
+  const handleDelete = (id) => {
+    const updatedTasks = originalData.filter((task) => task.id !== id);
+    setOriginaldata(updatedTasks);
+    setFilteredData(updatedTasks);
+  };
+  
 
   return (
-    <Card className = {classes.tasks}>
-          <select id="status" value={status} onChange={filterStatusHandler} ref={statusInputRef}>
-            <option value="Active">Active</option>
-            <option value="In Progress">In Progress</option>
-            <option value="Complete">Complete</option>
-          </select>
+    <Card className={classes.tasks}>
+      <select
+        id="status"
+        value={status}
+        onChange={filterStatusHandler}
+        ref={statusInputRef}
+      >
+        <option value="Active">Active</option>
+        <option value="In Progress">In Progress</option>
+        <option value="Complete">Complete</option>
+      </select>
 
-          {check ? <ul key={props.id}>
-      {filteredData.map(task =>(
-      <li>
-          <b>{task.title}</b>   {task.status}   {task.description}   {task.date}
-      </li>
-      ))}
-    </ul>:  <ul key={props.id}>
-      {props.tasks.map(task =>(
-      <li>
-          <b>{task.title}</b>   {task.status}   {task.description}   {task.date}
-      </li>
-      ))}
-    </ul>}
+      {check ? (
+        <ul key={props.id}>
+          {filteredData.map((task) => (
+            <li>
+              <b>{task.title}</b> {task.status} {task.description} {task.date}{" "}
+              <Button>update</Button> <Button onClick={() => handleDelete(task.id)}>remove</Button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <ul key={props.id}>
+          {originalData.map((task) => (
+            <li>
+              <b>{task.title}</b> {task.status} {task.description} {task.date}{" "}
+              <Button>update</Button> <Button onClick={() => handleDelete(task.id)}>remove</Button>
+            </li>
+          ))}
+        </ul>
+      )}
     </Card>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
